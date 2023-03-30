3a9d45ec462ee4fe2bb8591d2abd955f3751222f
Renato Rossi <renadlerossi+git@gmail.com>
2023-03-30 15:23:34 -0300

    feat: Create Style Variables file and add extra css to body in index.css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import styled from '@emotion/styled';
+
+import Hero from './components/Hero/Hero';
+import { Spaces } from './styles/variables';
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 ${Spaces.large};
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Main>
+            <Hero />
+        </Main>
+    );
 }
 
 export default App;
---
+++ src/styles/variables.ts
@@ -0,0 +1,24 @@
+export const Colors = {
+    primary: {
+        white: '#fff',
+        darkGray: '#a9a9a9',
+        lightBlue: '#dff6ff',
+        darkBlue: '#06283D'
+    }
+};
+
+export const Spaces = {
+    zero: '0',
+    xSmall: '6px',
+    small: '12px',
+    medium: '24px',
+    mLarge: '32px',
+    large: '48px',
+};
+
+export const FontSizes = {
+    mSmall: '16px',
+    medium: '20px',
+    mLarge: '22px',
+    large: '70px'
+};
