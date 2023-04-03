b2b1b95de48d0b2a959973f29398c2e298efb3c0
Renato Rossi <renadlerossi+git@gmail.com>
2023-04-03 10:50:29 -0300

    feat: Create Unit Test for App.tsx.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders App initial state', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heroHeader = screen.getByText('Shortly');
+  expect(heroHeader).toBeInTheDocument();
 });
